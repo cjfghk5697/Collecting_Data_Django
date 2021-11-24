9edc032db714ba42e740fa05b69ed9963044fcbd
cjfghk5697 <cjfghk5697@gmail.com>
2021-11-24 19:18:58 +0900

    make templates

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from datetime import datetime
+from . import models
 # Create your views here.
 
 
 def all_cats(request):
+
     now = datetime.now()
-    hungry = False
+    all_cats = models.Photo.objects.all().count()
     return render(request, "input.html", context={
         'now': now,
-        'hungry': hungry
+        'all_cats': all_cats,
     })
