7fca77703552b9657a6950e96732587f7965d2f1
cjfghk5697 <cjfghk5697@gmail.com>
2021-11-25 12:15:52 +0900

    seed photos

--- photos/management/commands/seed_photos.py
+++ photos/management/commands/seed_photos.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand
 from photos.models import Photo as photo_model
 from django_seed import Seed
+from django.contrib.admin.utils import flatten
+from users import models as user_models
+import random
 
 
 class Command(BaseCommand):
@@ -13,8 +16,30 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         number = options.get("number", 1)
 
+        all_users = user_models.User.objects.all()
+        CAT_HAK = "학치"
+        CAT_BBI = "삐약이"
+        CAT_MOK = "목주"
+        CAT_UNKNOWN = "모름"
+
+        CAT_CHOICES = [CAT_HAK,
+                       CAT_BBI,
+                       CAT_MOK,
+                       CAT_UNKNOWN,
+                       ]
         seeder = Seed.seeder()
-        seeder.add_entity(photo_model.Photo, number)
+        seeder.add_entity(photo_model, number, {
+            "cat_name": lambda x: random.choice(CAT_CHOICES),
+            "host": lambda x: random.choice(all_users),
+            "description": lambda x: seeder.faker.sentence(),
+        })
         # python manage.py seed_users --numbers 50
-        seeder.execute()
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+        for pk in created_clean:
+            photo = photo_model.objects.get(pk=pk)
+            for i in range(3, random.randint(10, 30)):
+                photo_model.objects.create(
+                    file=f"room_photos/{random.randint(1, 31)}.webp",
+                )
         self.stdout.write(self.style.SUCCESS(f'{number} photos created'))
