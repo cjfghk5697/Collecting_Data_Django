1fd70d276bd333c5796cd9be3489bba6c4a7528e
cjfghk5697 <cjfghk5697@gmail.com>
2021-12-02 23:29:58 +0900

    searchview class

--- photos/forms.py
+++ photos/forms.py
@@ -1,26 +1,6 @@
-"""from typing_extensions import Required
 from django import forms
 from . import models
-from .models import Photo
 
 
-class PhotoForm(forms.Form):
-    CAT_HAK = "학치"
-    CAT_BBI = "삐약이"
-    CAT_MOK = "목주"
-    CAT_UNKNOWN = "모름"
-    CAT_CHOICES = (
-        (CAT_HAK, "학치"),
-        (CAT_BBI, "삐약이"),
-        (CAT_MOK, "목주"),
-        (CAT_UNKNOWN, "모름"),
-    )
-    cat_name = models.CharField(
-        choices=CAT_CHOICES, max_length=4, required=False)
-    description = models.TextField(required=False)
-    file = models.ImageField(upload_to="cat_photos",
-                             null=True, blank=True, required=False)
-
-    class Meta:
-        model = Photo
-        fields = ['cat_name', 'description', 'file']"""
+class SearchForm(forms.Form):
+    cat = forms.CharField(initial="Anycat")
--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
-from . import models
+from . import models, forms
 
 
 class HomeView(ListView):
@@ -9,7 +9,6 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
-
     context_object_name = "photos"
 
 
@@ -18,17 +17,22 @@ class PhotoDetail(DetailView):
     model = models.Photo
 
 
-def search(request):
-    cat_name = request.GET.get("cat_name", "Anycat")
-    cat_name = str.capitalize(cat_name)
+class SearchView(View):
+    """ SearchView Definition """
 
-    filter_args = {}
-    form = {
-        "cat_name": cat_name,
-    }
+    def get(self, request):
 
-    if cat_name != "Anycat":
-        filter_args["cat_name"] = cat_name
-    photos = models.Photo.objects.filter(**filter_args)
+        form = forms.SearchForm(request.GET)
 
-    return render(request, "photos/search.html", {**form, "photos": photo1})
+        if form.is_valid():
+
+            cat = form.cleaned_data.get("cat")
+            filter_args = {}
+
+            if cat != "Anywhere":
+                filter_args["cat__startswith"] = cat
+            photos = models.Photo.objects.filter(**filter_args)
+        else:
+            form = forms.SearchForm()
+
+        return render(request, "photos/search.html", {"form": form, "photos": photos})
